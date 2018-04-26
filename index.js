9bfaf9b9ddcd34cceae8810857c26cc8c6ff19cf
AMKlein2018 <adamklein0519@gmail.com>
2018-04-26 11:08:49 -0400

    codechallenge

---
+++ index.js
@@ -0,0 +1,43 @@
+// // Code Challenge 1
+// Write a function that takes two numbers as arguments 
+// and prints the in the console the multiplied sum of 
+// the two. However, if you pass a negative 
+// number it prints "negative numbers are not allowed".
+
+// function multiplied (x, y) {
+// 	var result = (x * y) 
+// 	 if (result < 0 || result < 0) {
+// 		console.log ("negative numbers are not allowed")
+// 		} else {
+// 		console.log(result)
+// 	}
+
+// }
+
+// Code Challenge 2
+
+// Write a function that prints all the numbers from 1 to 100; 
+// if a number is divisible by 3 or 4 add the line 'this is divisible by 3 or 4'.
+
+
+var = i;
+var text = ""
+
+function allnumbers (array) {	
+
+	for (i = 0; i <= 100; i++) {
+		console.log (result)
+
+	if (i % 3 || i % 4) {
+		console.log("this is divisible by 3 or 4")
+	} else {
+		console.log ()
+	}
+
+}
+
+
+
+
+Code 
+
